4cc3976b6c47f5b1eb7a596eb02087391a06860f
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2023-01-24 12:09:15 +0200

    First update

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,129 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Diagnostics;
+using System.Web;
+using System;
+using System.Diagnostics;
+
+
+namespace SeleniumTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+
+                Console.Write("Enter Meeting url : ");
+                string url = Console.ReadLine();
+                Console.Write("Enter Meeting length(min) : ");
+                string length = Console.ReadLine();
+                int lengthMill = int.Parse(length) * 60 * 1000;
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = @"C:\\Program Files\\obs-studio\\bin\\64bit\\obs64.exe",
+              
+                    ArgumentList = {"--startrecording", "--minimize-to-tray", "--scene \"screen\"", "--multi" },
+                   WorkingDirectory = @"C:\\Program Files\\obs-studio\\bin\\64bit"
+                };
+                Process.Start(startInfo);
+              
+                Thread.Sleep(3000);
+                var uri = new Uri(url??"");
+                var wcValue = uri.Segments[2];
+                var pwdValue = HttpUtility.ParseQueryString(uri.Query).Get("pwd");
+                // Create a Chrome Driver object to open the browser 
+                IWebDriver driver = new ChromeDriver();
+                if (pwdValue != null)
+                {
+                    pwdValue = pwdValue.Replace("/join", "");
+                    // Navigate to the URL 
+                    driver.Navigate().GoToUrl($"https://us06web.zoom.us/wc/{wcValue}/join");
+                    driver.Manage().Window.FullScreen();
+                    IWebElement NameField = driver.FindElement(By.Id("inputname"));
+                    NameField.SendKeys("BitBang");
+                    IWebElement PassField = driver.FindElement(By.Id("inputpasscode"));
+                    PassField.SendKeys(pwdValue);
+                    IWebElement JoinButton = driver.FindElement(By.Id("joinBtn"));
+                    JoinButton.Click();
+                    Thread.Sleep(1000);
+                    IWebElement JoinButton2 = driver.FindElement(By.ClassName("preview-join-button"));
+                    JoinButton2.Click();
+                    driver.Manage().Window.FullScreen();
+
+                    Process obsProcess = Process.GetProcessesByName("obs64")[0];
+                    Process chromeProcess = Process.GetProcessesByName("chrome")[0];
+
+                    Thread.Sleep(lengthMill);
+                    obsProcess.Kill();
+                    chromeProcess.Kill();
+                }
+                else
+                {
+                    // Navigate to the URL 
+                    driver.Navigate().GoToUrl($"https://us06web.zoom.us/wc/{wcValue}/join");
+                    driver.Manage().Window.FullScreen();
+                    IWebElement NameField = driver.FindElement(By.Id("inputname"));
+                    NameField.SendKeys("BitBang");
+                    IWebElement JoinButton = driver.FindElement(By.Id("joinBtn"));
+                    JoinButton.Click();
+                    Thread.Sleep(1000);
+                    IWebElement JoinButton2 = driver.FindElement(By.ClassName("preview-join-button"));
+                    JoinButton2.Click();
+                    driver.Manage().Window.FullScreen();
+
+                    Process obsProcess = Process.GetProcessesByName("obs64")[0];
+                    Process chromeProcess = Process.GetProcessesByName("chrome")[0];
+
+                    Thread.Sleep(lengthMill);
+                    obsProcess.Kill();
+                    chromeProcess.Kill();
+                }
+
+
+               
+
+
+
+            }
+            catch (WebDriverException err)
+            {
+                Console.WriteLine("========================");
+                Console.WriteLine(err.Message);
+                Console.WriteLine("========================");
+
+            }
+
+        }
+
+
+       
+
+    }
+}
+
+
+
+
+
+/*
+using System.Diagnostics;
+
+string zoomDirectory = Environment.ExpandEnvironmentVariables(@"%APPDATA%\Zoom\bin");
+Console.Write("Please enter url : ");
+string input = Console.ReadLine();
+ProcessStartInfo startInfo = new ProcessStartInfo
+{
+    FileName = $@"{zoomDirectory}\Zoom.exe",
+    Arguments = $"--url={input}",
+    WorkingDirectory = zoomDirectory
+};
+Process.Start(startInfo);
+*/
---
+++ README.md
@@ -0,0 +1,12 @@
+## Zoom Auto Recorder
+
+### How to Use
+
+- Open exe
+- enter meeting url
+- enter meeting length (Will automatically close everything after this time)
+
+### Requirements
+
+1. [OBS Studio](https://obsproject.com/) is installed at
+2. [Chrome Last version](https://www.google.com/chrome/?brand=YTUH&gclid=Cj0KCQiA_bieBhDSARIsADU4zLfskODb5-pDrXrQpZqPhFrktOs0eCgHGI8SgHcKvxYlLLabUt7pEncaAqMgEALw_wcB&gclsrc=aw.ds)
